31163c8a10be97fc59f80953b6c5d6330e341544
Marcin Tomaszewski <87043088+martom93@users.noreply.github.com>
2023-01-25 02:40:48 +0100

    New Files

---
+++ Bit Manipulation/(190) Reverse Bits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        
+        #Declare the variable for our resault. resault contain 32-bit number
+        resault = 0
+
+        #We have to loop this 32 times, because our number has 32bits to check
+        for i in range(32):
+            #take the value of n-th bit AND it with logic 1
+            currentBit = (n >> i) & 1
+            #shift resault to the corect slot
+            resault = resault | currentBit << (31 - i)
+        
+        return resault
---
+++ Bit Manipulation/(268) Missing Number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        #Declare the lacking number variable
+        missing = 0
+        #Sort the array
+        nums = sorted(nums)
+
+        #Edge cases, if first element isn't 0 or the last element isn't N
+        if nums[len(nums)-1] != len(nums):
+            missing = len(nums)
+        if nums[0] != 0:
+            missing = 0
+
+        #Looping through array and checking difference between next and current element
+        #if it's 1, then check another pair, if not return missing element as: current + 1
+        for i in range(len(nums)-1):
+            if nums[i+1]-nums[i] != 1:
+                missing = nums[i] + 1
+        return missing
