f8cdeb48f06165768a2427c3323d7e437d8c1e39
Marcin Tomaszewski <87043088+martom93@users.noreply.github.com>
2023-03-24 15:38:35 +0100

    Update (190) Reverse Bits.py

--- Bit Manipulation/(190) Reverse Bits.py
+++ Bit Manipulation/(190) Reverse Bits.py
@@ -4,6 +4,7 @@ class Solution:
         #Declare the variable for our resault. resault contain 32-bit number
         resault = 0
 
+
         #We have to loop this 32 times, because our number has 32bits to check
         for i in range(32):
             #take the value of n-th bit AND it with logic 1
