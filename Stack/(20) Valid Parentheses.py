3fbbf188205c91ff7dd8e96d412834fddfdc449a
Marcin Tomaszewski <87043088+martom93@users.noreply.github.com>
2023-04-01 08:58:05 +0200

    Update (20) Valid Parentheses.py

--- Stack/(20) Valid Parentheses.py
+++ Stack/(20) Valid Parentheses.py
@@ -25,6 +25,5 @@ class Solution:
             else:
                 stack.append(element)
              
-  
         #Return True if the stack is empty, or False when it is not
         return True if not stack else False
