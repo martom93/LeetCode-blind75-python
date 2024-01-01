4732ecd3ad20fb762889e26e545f4c0270c7fdec
Marcin Tomaszewski <87043088+martom93@users.noreply.github.com>
2024-01-01 12:24:12 +0100

    Update (268) Missing Number.py

--- Bit Manipulation/(268) Missing Number.py
+++ Bit Manipulation/(268) Missing Number.py
@@ -11,7 +11,6 @@ class Solution:
         if nums[0] != 0:
             missing = 0
 
-
         #Looping through array and checking difference between next and current element
         #if it's 1, then check another pair, if not return missing element as: current + 1
         for i in range(len(nums)-1):
