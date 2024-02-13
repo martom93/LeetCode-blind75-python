925f4aab7416896ecee9fb00b51dd46566178861
Marcin Tomaszewski <87043088+martom93@users.noreply.github.com>
2024-02-13 16:13:36 +0100

    Update (125) Valid Palindrome.py

--- Two Pointers/(125) Valid Palindrome.py
+++ Two Pointers/(125) Valid Palindrome.py
@@ -13,9 +13,6 @@ class Solution:
                 right -= 1
 
 
-
-
-
             #if values aren't equal return False. Remember that we have to 
             #convert symbol to lowerCase.
             if s[left].lower() != s[right].lower():
