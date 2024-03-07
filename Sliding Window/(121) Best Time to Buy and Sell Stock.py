5e72d4d8d6eb8f37539a4afc0da33a5f60ad926a
Marcin Tomaszewski <87043088+martom93@users.noreply.github.com>
2024-03-07 22:35:13 +0100

    Update (121) Best Time to Buy and Sell Stock.py

--- Sliding Window/(121) Best Time to Buy and Sell Stock.py
+++ Sliding Window/(121) Best Time to Buy and Sell Stock.py
@@ -10,7 +10,6 @@ class Solution:
         #max profit
         maxProfit = 0
 
-
         #looping through until sell pointer reaches the edge of the array
         while sell < len(prices):
             #If we have the profit, calculate the profit and check if it's
