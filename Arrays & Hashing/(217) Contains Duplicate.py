2808ea65ec36a388a28145bfe7dcb523a962145e
Marcin Tomaszewski <87043088+martom93@users.noreply.github.com>
2024-04-06 22:28:13 +0200

    Update (217) Contains Duplicate.py

--- Arrays & Hashing/(217) Contains Duplicate.py
+++ Arrays & Hashing/(217) Contains Duplicate.py
@@ -1,10 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
-
         #Creating the set of unique element of array
         hashSet = set()
-
 
         #Iterate through each element of the array.
         #If element already exists, return false, if not
