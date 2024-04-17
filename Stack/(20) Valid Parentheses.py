84f519d88a9a61224aa8fc258e60c59dafc64216
Marcin Tomaszewski <87043088+martom93@users.noreply.github.com>
2024-04-17 23:47:43 +0200

    Update (20) Valid Parentheses.py

--- Stack/(20) Valid Parentheses.py
+++ Stack/(20) Valid Parentheses.py
@@ -4,7 +4,6 @@ class Solution:
         #Creating the stack to store parentheses
         stack = []
 
-
         #Creating hashmap containing matching parentheses
         openToClose = {
             "}":"{",
