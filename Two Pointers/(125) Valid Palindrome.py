1b6be337d513a2682ce82c3b11e18aad86e4b279
Marcin Tomaszewski <87043088+martom93@users.noreply.github.com>
2024-05-10 23:50:46 +0200

    Update (125) Valid Palindrome.py

--- Two Pointers/(125) Valid Palindrome.py
+++ Two Pointers/(125) Valid Palindrome.py
@@ -3,8 +3,6 @@ class Solution:
         #Declaration of left and right pointers on first and last element
         left, right = 0, len(s)-1
 
-
-
         #Iterate through the string until pointers meets each other
         while left<right:
             #while pointer doesnt show on Alfa-numeric symbol, increment or decrement
@@ -13,20 +11,14 @@ class Solution:
             while right>left and not self.checkIfAlNum(s[right]):
                 right -= 1
 
-
-
             #if values aren't equal return False. Remember that we have to 
             #convert symbol to lowerCase.
             if s[left].lower() != s[right].lower():
                 return False
 
-
             #Update the indexes. Increment left one, and decrement the right one.     
             left, right = left + 1, right - 1
         return True
-
-
-
 
     #function that checks the ASCII values of a-z, A-Z and 0-9 characters.
     def checkIfAlNum(self, element):
