dd7df52708bd4fcf2c62e397703ce47fe9468281
Marcin Tomaszewski <87043088+martom93@users.noreply.github.com>
2024-05-23 23:50:12 +0200

    Update (191) Number of 1 Bits.py

--- Bit Manipulation/(191) Number of 1 Bits.py
+++ Bit Manipulation/(191) Number of 1 Bits.py
@@ -2,11 +2,8 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         #Resault variable to keep track of 1's bits
         resault = 0
-
         #Keep looping until n contains only 0's
         while n:    
-
-
 
             #checking if our last bit is 1, then
             #shift the number by one using >>
@@ -14,9 +11,6 @@ class Solution:
                 resault += 1
                 n = n >> 1
             else: 
-
-
-
 
                 n = n >> 1
         return resault
