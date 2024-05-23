e8620cee4fb2a4e88787ee25f4a753719abc091e
Marcin Tomaszewski <87043088+martom93@users.noreply.github.com>
2024-05-23 23:49:33 +0200

    Update (191) Number of 1 Bits.py

--- Bit Manipulation/(191) Number of 1 Bits.py
+++ Bit Manipulation/(191) Number of 1 Bits.py
@@ -6,7 +6,6 @@ class Solution:
         #Keep looping until n contains only 0's
         while n:    
 
-
             #checking if our last bit is 1, then
             #shift the number by one using >>
             if n % 2 == 1:
